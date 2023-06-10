2d4b04db810bf8907d448bae8300ede042ff41c2
rsbisne <roshansb99@gmail.com>
2023-06-10 15:54:57 +0530

    added animation

--- src/components/shows/ShowCard.jsx
+++ src/components/shows/ShowCard.jsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import styled from 'styled-components';
 import { SearchCard, SearchImgWrapper } from '../common/SearchCard';
 import { StarIcon } from '../common/StarIcon';
@@ -5,6 +6,17 @@ const ShowCard = ({ name, image, summary, id, onStarMeClick, isStarred }) => {
   const summaryStripped = summary
     ? summary.split(' ').slice(0, 10).join(' ').replace(/<.+?>/g, '') + '...'
     : 'No description';
+  const starBtnRef = useRef();
+  const handleStarClick = () => {
+    onStarMeClick(id);
+    const starBtnE1 = starBtnRef.current;
+    if (!starBtnE1) return;
+    if (isStarred) {
+      starBtnE1.classList.remove('animate');
+    } else {
+      starBtnE1.classList.add('animate');
+    }
+  };
   return (
     <SearchCard>
       <SearchImgWrapper>
@@ -16,7 +28,7 @@ const ShowCard = ({ name, image, summary, id, onStarMeClick, isStarred }) => {
         <a href={`/show/${id}`} target="_blank" rel="noreferrer">
           Read more
         </a>
-        <StarBtn type="button" onClick={() => onStarMeClick(id)}>
+        <StarBtn ref={starBtnRef} type="button" onClick={handleStarClick}>
           <StarIcon active={isStarred} />
         </StarBtn>
       </ActionSection>
@@ -50,5 +62,21 @@ const StarBtn = styled.button`
   align-items: center;
   &:hover {
     cursor: pointer;
+  }
+  &.animate {
+    ${StarIcon} {
+      animation: increase 0.75s ease-in forwards;
+      @keyframes increase {
+        0% {
+          transform: scale(1);
+        }
+        50% {
+          transform: scale(3) rotate(45deg);
+        }
+        100% {
+          transform: scale(1);
+        }
+      }
+    }
   }
 `;
