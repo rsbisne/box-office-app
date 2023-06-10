aa1f95df4d0d74ee8b1649c2c1a220bf08ca9115
rsbisne <roshansb99@gmail.com>
2023-06-11 00:07:16 +0530

    Error in the link connection

--- src/components/shows/ShowCard.jsx
+++ src/components/shows/ShowCard.jsx
@@ -26,7 +26,7 @@ const ShowCard = ({ name, image, summary, id, onStarMeClick, isStarred }) => {
       <h1> {name} </h1>
       <p> {summaryStripped}</p>
       <ActionSection>
-        <Link href={`/show/${id}`} target="_blank" rel="noreferrer">
+        <Link to={`/show/${id}`} target="_blank" rel="noreferrer">
           Read more
         </Link>
         <StarBtn ref={starBtnRef} type="button" onClick={handleStarClick}>
